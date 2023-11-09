6bdc406727d93919a4e90f0ba11aa98e97c01600
alecanonm <al3jandrocan0n@gmail.com>
2023-11-09 10:42:44 -0500

    adding searchbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
         <motion.section
           animate={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
-          className=" fixed flex bg-[#ffffff] dark:bg-black bottom-0 left-0 w-full h-screen items-center justify-center"
+          className=" fixed flex bg-[#ffffff] bottom-0 left-0 w-full h-screen items-center justify-center"
         >
           <motion.section
             variants={navMotion}

45825ac9101e953c7a2c1ece3194abd05a50551a
alecanonm <al3jandrocan0n@gmail.com>
2023-11-16 11:32:17 -0500

    adding register section

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,11 +1,14 @@
 import logo from "../../assets/vaper-logo.png";
-
+import google from "../../assets/google.png";
+import { Link } from "react-router-dom";
 const Signin = () => {
   return (
     <>
       <form action="" className="flex flex-col  items-center">
         <figure>
-          <img src={logo} alt="logo" width={200} />
+          <Link to={"/"}>
+            <img src={logo} alt="logo" width={200} height={200} />
+          </Link>
         </figure>
         <h1 className=" text-center pb-5 text-2xl font-bold -translate-y-5">
           Nos llena de felicidad tenerte como uno de nuestros valiosos clientes!
@@ -43,7 +46,15 @@ const Signin = () => {
             className=" p-1.5 w-72 max-sm:w-80  rounded-md  border-2 border-black "
           />
         </label>
-        <button className=" p-4">Registrarse</button>
+        <div className="p-4 text-center ">
+          <button className="pb-2">Registrarse</button>
+          <div className=" flex items-center justify-center gap-3">
+            <p className=" text-slate-400">Continuar con </p>
+            <div role="button">
+              <img src={google} alt="google" width={18} height={18} />
+            </div>
+          </div>
+        </div>
       </form>
     </>
   );

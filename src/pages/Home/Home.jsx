351f5a8a9c520e21c303028c3f0060aa1a4cd0f0
alecanonm <al3jandrocan0n@gmail.com>
2023-11-18 12:20:15 -0500

    adding another section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import mussblack from "../../assets/mussblack (2).png";
+import lostMary from "../../assets/pineappleice-removebg-preview (1).png";
 const Home = () => {
   return (
     <div className="">
@@ -18,7 +19,22 @@ const Home = () => {
           Saber mas
         </button>
       </section>
-      <section></section>
+      <section className=" max-sm:flex-col  flex justify-center items-center h-screen  bg-[#a36969] text-white">
+        <div className=" flex flex-col gap-10">
+          <h1 className=" uppercase font-bold text-4xl text-center">
+            Lost Mary Bm 600
+          </h1>
+          <button className=" max-sm:hidden uppercase font-semibold hover:scale-110 transition p-2 w-64 rounded-lg  bg-[#7474745d] self-center ">
+            Saber mas
+          </button>
+        </div>
+        <figure>
+          <img src={lostMary} alt="lost mary" width={500} height={500} />
+        </figure>
+        <button className=" max-sm:block hidden uppercase font-semibold p-2 w-64 rounded-lg  bg-[#7474745d]">
+          Saber mas
+        </button>
+      </section>
       <section></section>
     </div>
   );

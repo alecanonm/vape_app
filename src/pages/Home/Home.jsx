185f59c3a4274f95f925e8be9275b617d8b0652f
alecanonm <al3jandrocan0n@gmail.com>
2023-11-17 19:12:32 -0500

    adding content to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import mussblack from "../../assets/mussblack (2).png";
 import smoke from "../../assets/somke.png";
+import { useState } from "react";
 const Home = () => {
+  const [matches] = useState(window.matchMedia("(max-width: 900px)").matches);
   return (
     <div className="">
       <section className=" max-xl:flex-col  flex justify-center items-center h-screen  bg-black text-white">
-        <div className=" flex flex-col">
+        <div className=" flex flex-col gap-10">
           <h1 className=" uppercase font-bold text-4xl">Muss Marmol 700</h1>
+          {!matches && (
+            <button className=" uppercase font-semibold hover:scale-110 transition p-2 w-64 rounded-lg  bg-[#7474745d] self-center ">
+              Saber mas
+            </button>
+          )}
         </div>
         <figure>
           <img
@@ -16,6 +23,11 @@ const Home = () => {
           />
           <img src={mussblack} alt="mussblack" width={600} height={600} />
         </figure>
+        {matches && (
+          <button className=" uppercase font-semibold p-2 w-64 rounded-lg  bg-[#7474745d]">
+            Saber mas
+          </button>
+        )}
       </section>
       <section></section>
       <section></section>

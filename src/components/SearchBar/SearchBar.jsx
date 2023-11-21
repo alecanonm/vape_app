ccb13172d0547d579d1f6dcd2dce1c73c52a2442
alecanonm <al3jandrocan0n@gmail.com>
2023-11-20 23:19:20 -0500

    adding new design

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 const SearchBar = () => {
   return (
     <input
-      className=" bg-[#05050568] ring-1 ring-slate-950 fle placeholder:text-black rounded-lg text-center p-0.5 w-96 max-[1020px]:w-60"
+      className=" bg-[#ffffffa2] backdrop-blur-sm ring-1 ring-slate-950 flex placeholder:text-black rounded-lg text-center p-0.5 w-96 max-[1020px]:w-60"
       type="text"
       placeholder="Buscar"
     />
